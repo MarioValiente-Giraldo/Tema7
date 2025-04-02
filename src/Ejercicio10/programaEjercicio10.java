bf656191291f5c6264e1c9a4bf59c9994c706044
mariovaliente0 <mariovaliente0@gmail.com>
2025-04-02 13:35:57 +0200

    Añadir ejercicio 10 terminado

--- src/Ejercicio10/programaEjercicio10.java
+++ src/Ejercicio10/programaEjercicio10.java
@@ -8,7 +8,19 @@ public class programaEjercicio10 {
         BiFunction<Integer,Integer,Double> a  = (Integer correctas,Integer totalPreguntas) -> (((double)correctas/totalPreguntas)*10);
         System.out.println(a.apply(coreccionExamen.respuestasCorrectas(), coreccionExamen.numeroPreguntas()));
 
+        BiFunction<Integer,Integer,Double> b  = (Integer correctas,Integer totalPreguntas) ->(((double)(coreccionExamen.respuestasCorrectas() - coreccionExamen.respuestasIncorrectas())/totalPreguntas)*10);
 
-        }
+        System.out.println(b.apply(coreccionExamen.respuestasCorrectas(), coreccionExamen.numeroPreguntas()));
+
+        BiFunction<Integer,Integer,Double> c  = (Integer correctas,Integer totalPreguntas) ->{
+        int penalizacion = (coreccionExamen.respuestasCorrectas() - coreccionExamen.respuestasIncorrectas())/2;
+            return (((double)penalizacion/totalPreguntas)*10);
+        };
+        System.out.println(c.apply(coreccionExamen.respuestasCorrectas(), coreccionExamen.numeroPreguntas()));
+
+
+
+
+    }
     }
 
